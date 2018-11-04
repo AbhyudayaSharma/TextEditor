dd1fbccf51e13b6e07bf088dbff9456deaa7789b
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-11-04 13:46:59 +0530

    Add option to save and open files

--- src/main/java/Editor.java
+++ src/main/java/Editor.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import javax.swing.text.DefaultStyledDocument;
 import java.awt.*;
+import java.io.*;
 import java.util.StringTokenizer;
 
 class Editor extends JPanel {
     private JTextPane textPane;
 
+    final static String FILE_EXTENSION = ".std";
+
     Editor() {
         super();
         textPane = new JTextPane();
-        textPane.setContentType("text/html");
-        textPane.setText("<html><body>This is some random text.</body></html>");
+        textPane.setText("");
 
         // no soft-wraps
         var noWrapPanel = new JPanel(new BorderLayout());
@@ -19,6 +22,7 @@ class Editor extends JPanel {
         var scrollPane = new JScrollPane(noWrapPanel);
         textPane.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
 
+        System.out.println(textPane.getStyledDocument().getClass().getCanonicalName());
         // add the scrollPane
         setLayout(new GridLayout(1, 1));
         add(scrollPane);
@@ -30,7 +34,47 @@ class Editor extends JPanel {
      * @return the number of words
      */
     int wordCount() {
-        System.out.println(textPane.getText());
-        return new StringTokenizer(textPane.getText(), " ,.-").countTokens();
+        return new StringTokenizer(textPane.getText(), " ,.!?/\\()[]{};:").countTokens();
+    }
+
+    /**
+     * Saves the data in the editor to a file
+     *
+     * @param file data is written to this file
+     * @throws IOException when unable to write the file
+     */
+    void save(File file) throws IOException {
+        var path = file.getAbsolutePath();
+        // The file should have the extension applied if it doesn't already
+        if (!path.endsWith(FILE_EXTENSION)) {
+            path += FILE_EXTENSION;
+        }
+
+        try (var oos = new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeObject(textPane.getStyledDocument());
+        }
+    }
+
+
+    /**
+     * Opens a supported Styled Document Format file
+     *
+     * @param file the Styled Document File to be opened.
+     * @throws IOException when unable to open the file
+     */
+    void open(File file) throws IOException {
+        if (textPane.getText().length() != 0) {
+            int response = JOptionPane.showOptionDialog(getTopLevelAncestor(), "You have data in you editor. " +
+                            "You will lose it when you load a new file. Do you want to continue?", "Warning",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, JOptionPane.NO_OPTION);
+            if (response == JOptionPane.YES_OPTION) {
+                try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                    var document = (DefaultStyledDocument) ois.readObject();
+                    textPane.setStyledDocument(document);
+                } catch (ClassNotFoundException | IOException e) {
+                    throw new IOException("Unsupported file format!");
+                }
+            }
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.InputEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,9 +11,62 @@ public class Main {
             final var frame = new JFrame("TextEditor");
             final var editor = new Editor();
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.setLayout(new GridBagLayout());
+            frame.setLayout(new GridBagLayout())
+            ;
             JMenuBar menuBar = new JMenuBar();
-            menuBar.add(new JMenu("File"));
+            JMenu fileMenu = new JMenu("File");
+            JMenu editMenu = new JMenu("Edit");
+
+            JMenuItem saveButton = new JMenuItem("Save...");
+            saveButton.setAccelerator(KeyStroke.getKeyStroke('S', InputEvent.CTRL_DOWN_MASK));
+            saveButton.addActionListener(e -> {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileFilter(new FileNameExtensionFilter("Styled documents",
+                        Editor.FILE_EXTENSION.substring(1))); // FileNameExtensionFilter doesn't want a '.'
+                fileChooser.showDialog(frame, "Save");
+                File file = fileChooser.getSelectedFile();
+                if (file != null) {
+                    try {
+                        editor.save(file);
+                        JOptionPane.showMessageDialog(frame, "File written successfully!", "Information",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+
+            JMenuItem openButton = new JMenuItem("Open...");
+            openButton.setAccelerator(KeyStroke.getKeyStroke('O', InputEvent.CTRL_DOWN_MASK));
+            openButton.addActionListener(e -> {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setFileFilter(new FileNameExtensionFilter("Styled documents",
+                        Editor.FILE_EXTENSION.substring(1))); // FileNameExtensionFilter doesn't want a '.'
+                fileChooser.showDialog(frame, "Open");
+                File file = fileChooser.getSelectedFile();
+                if (file != null) {
+                    try {
+                        editor.open(file);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+
+            JMenuItem cutButton = new JMenuItem("Cut");
+            JMenuItem copyButton = new JMenuItem("Copy");
+            JMenuItem pasteButton = new JMenuItem("Paste");
+
+            fileMenu.add(openButton);
+            fileMenu.add(new JSeparator());
+            fileMenu.add(saveButton);
+
+            editMenu.add(cutButton);
+            editMenu.add(copyButton);
+            editMenu.add(pasteButton);
+
+            menuBar.add(fileMenu);
+            menuBar.add(editMenu);
             frame.setJMenuBar(menuBar);
 
             GridBagConstraints gbc = new GridBagConstraints();
@@ -22,8 +79,8 @@ public class Main {
             gbc.fill = 1;
             gbc.insets = new Insets(1, 1, 1, 1);
             frame.add(editor, gbc);
-            System.out.println(editor.wordCount());
 
+            frame.setPreferredSize(new Dimension(500, 500));
             frame.pack();
             frame.setVisible(true);
         });
