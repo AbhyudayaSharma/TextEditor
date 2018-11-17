ce27ba352603384a1d42ca2b6411eaf969120e51
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2018-11-17 13:51:36 +0530

    Make the oval selectable
    Now looks just like MS Word

--- src/main/java/drawing/OvalPanel.java
+++ src/main/java/drawing/OvalPanel.java
@@ -1,40 +1,53 @@
 package drawing;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * A panel which draws an oval. The size and position of the oval can be
  * modified by using the mouse.
  */
 public class OvalPanel extends RectanglePanel {
+    private boolean isOvalSelected = false;
+
     /**
      * Creates an {@link OvalPanel}. Since an oval is drawn just like a {@link Rectangle} in AWT,
      * we can reuse the mouse motions used in {@link RectanglePanel} to draw the oval.
      */
     OvalPanel() {
         super();
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                isOvalSelected = getRectangle().contains(e.getPoint());
+                repaint();
+            }
+        });
     }
 
     @Override
     protected void draw(Graphics g) {
-        g.drawString("Use the red circle to move the oval.\n", 10, 20);
+        g.drawString("Click on the oval to select it. Use the red circle to move the oval.", 10, 20);
         g.drawString("Use the blue circles to change its dimensions.", 10, 35);
         var rect = super.getRectangle();
         g.fillOval(rect.x, rect.y, rect.width, rect.height);
 
-        // draw corner hints
-        g.setColor(Color.LIGHT_GRAY);
-        g.drawRect(rect.x, rect.y, rect.width, rect.height);
-        g.setColor(Color.BLUE);
-        g.drawOval(rect.x, rect.y, 5, 5); // remember something? Hamming codes?
-        g.drawOval(rect.x, rect.y + rect.height - 5, 5, 5);
-        g.drawOval(rect.x + rect.width - 5, rect.y + rect.height - 5, 5, 5);
-        g.drawOval(rect.x + rect.width - 5, rect.y, 5, 5);
+        if (isOvalSelected) {
+            // draw corner hints
+            g.setColor(Color.LIGHT_GRAY);
+            g.drawRect(rect.x, rect.y, rect.width, rect.height);
+            g.setColor(Color.BLUE);
+            g.drawOval(rect.x, rect.y, 5, 5); // remember something? Hamming codes?
+            g.drawOval(rect.x, rect.y + rect.height - 5, 5, 5);
+            g.drawOval(rect.x + rect.width - 5, rect.y + rect.height - 5, 5, 5);
+            g.drawOval(rect.x + rect.width - 5, rect.y, 5, 5);
 
-        // draw center hints
-        g.setColor(Color.RED);
-        g.fillOval((int) rect.getCenterX(), (int) rect.getCenterY(), 5, 5);
-        g.setColor(Color.BLACK);
-
+            // draw center hints
+            g.setColor(Color.RED);
+            g.fillOval((int) rect.getCenterX(), (int) rect.getCenterY(), 5, 5);
+            g.setColor(Color.BLACK);
+        }
     }
 }
